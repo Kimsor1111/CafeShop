0dcfe48db6dd9d43624c96604460d65cfe778516
Kimsor1111 <angkimsor@gmail.com>
2025-01-08 13:21:34 +0700

    hello

--- src/component/Home/featureMid.jsx
+++ src/component/Home/featureMid.jsx
@@ -35,14 +35,14 @@ const featureMid = ({ title, des, img, featureindex }) => {
             <div className="lg:w-[62%] w-full h-full">
               <img src={img} alt="" className="w-full h-full object-cover " />
             </div>
-            <div className="lg:w-[38%] lg:relative lg:items-center lg:bg-[rgb(71,51,38)] items-end bg-black/50 absolute w-full right-0 h-full flex flex-col justify-center px-5">
+            <div className="lg:w-[38%] lg:relative lg:items-center lg:bg-[rgb(71,51,38)] bg-black/50 absolute w-full right-0 h-full flex flex-col justify-center px-5">
               <h1 className="lg:text-5xl text-3xl font-['poppins'] font-bold text-white">
                 {title}
               </h1>
-              <h1 className="lg:w-fit lg:pl-0 text-md text-sm font-['poppins'] text-white my-5">
+              <h1 className="lg:w-fit  text-md text-sm font-['poppins'] text-white my-5">
                 {des}
               </h1>
-              <div className="items-center w-full h-[45px] flex lg:justify-start justify-start">
+              <div className="items-center w-full h-[45px] flex">
                 <button className="lg:px-16 px-10 w-fit h-full bg-[rgba(253,240,224,255)] text-xl font-bold font-['inter'] rounded-[25px]">
                   Find Out
                 </button>
